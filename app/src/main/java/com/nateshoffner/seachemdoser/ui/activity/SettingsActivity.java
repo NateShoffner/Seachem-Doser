0c2f1b0f30614f979dcfa8e243afb3f6d93032cf
Nate Shoffner <nate.shoffner@gmail.com>
2016-03-11 10:52:41 -0500

    Added Play Store rating option to settings

--- app/src/main/java/com/nateshoffner/seachemdoser/ui/activity/SettingsActivity.java
+++ app/src/main/java/com/nateshoffner/seachemdoser/ui/activity/SettingsActivity.java
@@ -1,6 +1,9 @@
 package com.nateshoffner.seachemdoser.ui.activity;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.Preference;
@@ -42,6 +45,7 @@ public class SettingsActivity extends PreferenceActivity
 
         findPreference("app_version").setSummary("v" + BuildConfig.VERSION_NAME);
         findPreference("about_changelog").setOnPreferenceClickListener(this);
+        findPreference("about_rate").setOnPreferenceClickListener(this);
         updatePreferenceSummary(getString(R.string.pref_unit_measurement), null);
         mSharedPreferences.registerOnSharedPreferenceChangeListener(this);
     }
@@ -52,6 +56,23 @@ public class SettingsActivity extends PreferenceActivity
             DoserChangelog cl = new DoserChangelog(this);
             cl.getFullLogDialog().show();
             return true;
+        }
+
+        if (preference.getKey().equals("about_rate")) {
+            Uri uri = Uri.parse("market://details?id=" + this.getPackageName());
+            Intent goToMarket = new Intent(Intent.ACTION_VIEW, uri);
+            // To count with Play market backstack, After pressing back button,
+            // to taken back to our application, we need to add following flags to intent.
+            goToMarket.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY |
+                    Intent.FLAG_ACTIVITY_NEW_DOCUMENT |
+                    Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
+            try {
+                startActivity(goToMarket);
+            } catch (ActivityNotFoundException e) {
+                startActivity(new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("http://play.google.com/store/apps/details?id=" +
+                                this.getPackageName())));
+            }
         }
 
         return false;
