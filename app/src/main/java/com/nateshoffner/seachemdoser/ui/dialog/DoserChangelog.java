096e88ec5480f3d156919d82c988509a172e8767
Nate Shoffner <nate.shoffner@gmail.com>
2016-03-12 06:49:12 -0500

    Localized ChangeLog + fixed AppCompat title bug

--- app/src/main/java/com/nateshoffner/seachemdoser/ui/dialog/DoserChangelog.java
+++ app/src/main/java/com/nateshoffner/seachemdoser/ui/dialog/DoserChangelog.java
@@ -5,8 +5,6 @@ import android.view.ContextThemeWrapper;
 
 import com.nateshoffner.seachemdoser.R;
 
-import de.cketti.library.changelog.ChangeLog;
-
 public class DoserChangelog extends ChangeLog {
     public static final String DARK_THEME_CSS =
             "body { color: #FFFFFF; background-color: #282828; }" + "\n" + DEFAULT_CSS;
