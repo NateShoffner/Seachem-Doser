a82c0d85fa35c07554ce425f19af24151a11f202
Nate Shoffner <nate.shoffner@gmail.com>
2016-03-23 11:31:11 -0400

    Added Flourite Sand

---
+++ app/src/main/java/com/nateshoffner/seachemdoser/core/model/products/gravel/FlouriteSand.java
@@ -0,0 +1,18 @@
+package com.nateshoffner.seachemdoser.core.model.products.gravel;
+
+import com.nateshoffner.seachemdoser.DoserApplication;
+import com.nateshoffner.seachemdoser.R;
+import com.nateshoffner.seachemdoser.core.model.SeachemDosage;
+import com.nateshoffner.seachemdoser.core.model.UnitMeasurement;
+
+public class FlouriteSand extends Gravel {
+
+    public FlouriteSand() {
+        super(DoserApplication.getContext().getString(R.string.product_flourite_sand));
+    }
+
+    @Override
+    public SeachemDosage[] calculateDosage(UnitMeasurement unitMeasurement) {
+        return CalculateDosage(unitMeasurement, 260, 130);
+    }
+}
