1dfde939aaaee64e1c8e2e812bef895e2e9ed8af
Nate Shoffner <nate.shoffner@gmail.com>
2016-03-26 04:53:42 -0400

    Default fragment + drawer refactoring/icons

--- app/src/main/java/com/nateshoffner/seachemdoser/ui/activity/MainActivity.java
+++ app/src/main/java/com/nateshoffner/seachemdoser/ui/activity/MainActivity.java
@@ -3,83 +3,40 @@ package com.nateshoffner.seachemdoser.ui.activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.mikepenz.google_material_typeface_library.GoogleMaterial;
 import com.mikepenz.materialdrawer.AccountHeader;
 import com.mikepenz.materialdrawer.AccountHeaderBuilder;
 import com.mikepenz.materialdrawer.Drawer;
 import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.ExpandableDrawerItem;
 import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
 import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
 import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
 import com.mikepenz.materialdrawer.model.SectionDrawerItem;
 import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
-import com.mikepenz.materialdrawer.model.interfaces.IProfile;
 import com.mikepenz.materialdrawer.model.interfaces.Nameable;
 import com.nateshoffner.seachemdoser.DoserApplication;
 import com.nateshoffner.seachemdoser.R;
 import com.nateshoffner.seachemdoser.core.manager.SeachemManager;
 import com.nateshoffner.seachemdoser.core.model.SeachemProduct;
 import com.nateshoffner.seachemdoser.core.model.SeachemProductType;
+import com.nateshoffner.seachemdoser.ui.fragment.DefaultFragment;
 import com.nateshoffner.seachemdoser.ui.fragment.PreferencesFragment;
 import com.nateshoffner.seachemdoser.ui.fragment.ProductDetailFragment;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class MainActivity extends AppCompatActivity {
 
-    private class ProductTypeItem {
-
-        private IDrawerItem mItem;
-        private List<IDrawerItem> mChildren = new ArrayList<>();
-        private boolean mExpanded;
-
-        public ProductTypeItem(IDrawerItem item) {
-            mItem = item;
-        }
-
-        public IDrawerItem getItem() {
-            return mItem;
-        }
-
-        public void addChild(IDrawerItem item) {
-            mChildren.add(item);
-        }
-
-        public void setOpen(boolean open) {
-            mExpanded = open;
-        }
-
-        public boolean isExpanded() {
-            return mExpanded;
-        }
-
-        public void collapse(Drawer drawer) {
-            // convert to primitive types to pass as varargs
-            long[] ids = new long[mChildren.size()];
-            for (int i = 0, mChildrenIdsSize = mChildren.size(); i < mChildrenIdsSize; i++) {
-                ids[i] = mChildren.get(i).getIdentifier();
-            }
-
-            drawer.removeItems(ids);
-
-            mExpanded = false;
-        }
-
-        public void expand(Drawer drawer) {
-            int position =  drawer.getPosition(getItem().getIdentifier());
-            drawer.addItemsAtPosition(position + 1, mChildren.toArray(new IDrawerItem[mChildren.size()]));
-            mExpanded = true;
-        }
-    }
-
     private Drawer mDrawer = null;
-    private List<ProductTypeItem> mProductItems;
     private PreferencesFragment mPreferencesFragment = new PreferencesFragment();
 
     private final static long SETTINGS_ITEM_IDENTIFIER = 999;
@@ -93,46 +50,17 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         setSupportActionBar(toolbar);
 
-        int identifierIncrementor = 0;
-
-        List<SeachemProductType> productTypes = SeachemManager.GetProductTypes();
-
-        mProductItems = new ArrayList<>();
-
-        for (int i = 0; i < productTypes.size(); i++) {
-            SeachemProductType type = productTypes.get(i);
-
-            ProductTypeItem item = new ProductTypeItem(new PrimaryDrawerItem()
-                    .withName(getProductTypeString(type))
-                    .withIdentifier(identifierIncrementor++)
-                    .withLevel(2)
-                    .withSelectable(false));
-
-            List<SeachemProduct> products = SeachemManager.GetProducts(type);
-
-            for (SeachemProduct product : products) {
-                item.addChild(new SecondaryDrawerItem()
-                        .withName(product.getName())
-                        .withIdentifier(identifierIncrementor++)
-                        .withLevel(3));
-            }
-
-            mProductItems.add(item);
-        }
-
         AccountHeader headerResult = new AccountHeaderBuilder()
                 .withActivity(this)
+                .withCompactStyle(true)
                 .withHeaderBackground(R.drawable.action_bar_gradient)
                 .addProfiles(
-                        new ProfileDrawerItem().withName(getString(R.string.app_name)).withIcon(R.mipmap.ic_launcher)
+                        new ProfileDrawerItem()
+                                .withName(getString(R.string.app_name))
+                                .withIcon(R.mipmap.ic_launcher)
                 )
-                .withOnAccountHeaderListener(new AccountHeader.OnAccountHeaderListener() {
-                    @Override
-                    public boolean onProfileChanged(View view, IProfile profile, boolean currentProfile) {
-                        return false;
-                    }
-                })
                 .withSelectionListEnabledForSingleProfile(false)
+                .withProfileImagesClickable(false)
                 .build();
 
         DrawerBuilder builder = new DrawerBuilder()
@@ -142,35 +70,22 @@ public class MainActivity extends AppCompatActivity {
                 .withDelayOnDrawerClose(250)
                 .withActionBarDrawerToggle(true)
                 .withActionBarDrawerToggleAnimated(true)
-                .addDrawerItems(
-                        new SectionDrawerItem().withName("Products")
-
-                )
+                .withSelectedItem(-1)
+                .addDrawerItems(new SectionDrawerItem().withName(R.string.products))
                 .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
                     @Override
                     public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
-
-                        ProductTypeItem productTypeItem = getProductTypeItem(drawerItem.getIdentifier());
-                        if (productTypeItem != null) {
-                            if (productTypeItem.isExpanded()) {
-                                productTypeItem.collapse(mDrawer);
-                            } else {
-                                productTypeItem.expand(mDrawer);
-                            }
+                        if (drawerItem.getIdentifier() == SETTINGS_ITEM_IDENTIFIER) {
+                            setCurrentFragment(getString(R.string.action_settings), null,
+                                    mPreferencesFragment);
                         }
 
                         else {
-                            String title = ((Nameable)drawerItem).getName().getText();
-
+                            String title = ((Nameable) drawerItem).getName().getText();
                             SeachemProduct product = SeachemManager.getProductByName(title);
 
                             if (product != null) {
                                 showProduct(product);
-                            }
-
-                            else if (drawerItem.getIdentifier() == SETTINGS_ITEM_IDENTIFIER) {
-                                setCurrentFragment(getString(R.string.action_settings), null,
-                                        mPreferencesFragment);
                             }
                         }
 
@@ -179,24 +94,46 @@ public class MainActivity extends AppCompatActivity {
                 });
 
 
+        // populate product items
+        int identifierIncrementor = 0;
+        List<SeachemProductType> productTypes = SeachemManager.GetProductTypes();
 
-        for (ProductTypeItem item : mProductItems) {
-            builder.addDrawerItems(item.getItem());
+        for (SeachemProductType type : productTypes) {
+            ExpandableDrawerItem ex =  new ExpandableDrawerItem()
+                    .withName(getProductTypeString(type))
+                    .withIdentifier(identifierIncrementor++)
+                    .withSelectable(false)
+                    .withLevel(2)
+                    .withTextColor(getProductTypeColor(type));
+
+            List<SeachemProduct> products = SeachemManager.GetProducts(type);
+
+            for (SeachemProduct product : products) {
+                ex.withSubItems(new SecondaryDrawerItem()
+                        .withName(product.getName())
+                        .withIcon(GoogleMaterial.Icon.gmd_play_arrow)
+                        .withIdentifier(identifierIncrementor++)
+                        .withLevel(3));
+            }
+
+            builder.addDrawerItems(ex);
         }
 
         mDrawer = builder.build();
 
         PrimaryDrawerItem settingsItem = new PrimaryDrawerItem()
                 .withName(R.string.action_settings)
-                .withIdentifier(SETTINGS_ITEM_IDENTIFIER);
+                .withIdentifier(SETTINGS_ITEM_IDENTIFIER)
+                .withIcon(GoogleMaterial.Icon.gmd_settings);
         mDrawer.addStickyFooterItem(settingsItem);
 
         showDefaultProduct();
     }
 
     private void setCurrentFragment(String title, String subtitle, Fragment fragment) {
-        getSupportActionBar().setTitle(title);
-        getSupportActionBar().setSubtitle(subtitle);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle(title);
+        actionBar.setSubtitle(subtitle);
 
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.content, fragment)
@@ -206,13 +143,16 @@ public class MainActivity extends AppCompatActivity {
             mDrawer.closeDrawer();
     }
 
-    private ProductTypeItem getProductTypeItem(long id) {
-        for (ProductTypeItem item : mProductItems) {
-            if (item.getItem().getIdentifier() == id) {
-                return item;
-            }
+    private int getProductTypeColor(SeachemProductType type) {
+        switch (type) {
+            case Gravel:
+                return ContextCompat.getColor(this, R.color.product_type_gravel);
+            case Planted:
+                return ContextCompat.getColor(this, R.color.product_type_planted);
+            case Reef:
+                return ContextCompat.getColor(this, R.color.product_type_reef);
         }
-        return null;
+        return 0;
     }
 
     private String getProductTypeString(SeachemProductType type) {
@@ -234,6 +174,8 @@ public class MainActivity extends AppCompatActivity {
         ProductDetailFragment fragment = new ProductDetailFragment();
         fragment.setArguments(arguments);
         setCurrentFragment(getString(R.string.app_name), product.getName(), fragment);
+
+        DoserApplication.getDoserPreferences().setLastProduct(product);
     }
 
     private void showDefaultProduct() {
@@ -244,21 +186,28 @@ public class MainActivity extends AppCompatActivity {
 
             // first open parent item
             for (IDrawerItem item : mDrawer.getDrawerItems()) {
-                String title = ((Nameable)item).getName().getText();
-                if (title.equals(parentTitle)) {
-                    mDrawer.setSelection(item, true);
-                    break;
+                if (item instanceof ExpandableDrawerItem) {
+                    ExpandableDrawerItem expandableDrawerItem = (ExpandableDrawerItem)item;
+                    String title = ((Nameable) item).getName().getText();
+                    if (title != null && title.equals(parentTitle)) {
+                        expandableDrawerItem.withIsExpanded(true);
+                        break;
+                    }
                 }
             }
 
             // now select + click child item
             for (IDrawerItem item : mDrawer.getDrawerItems()) {
                 String title = ((Nameable)item).getName().getText();
-                if (title.equals(defaultProduct.getName())) {
+                if (title != null && title.equals(defaultProduct.getName())) {
                     mDrawer.setSelection(item, true);
                     break;
                 }
             }
+        }
+
+        else {
+            setCurrentFragment(getString(R.string.welcome), null, new DefaultFragment());
         }
     }
 
---
+++ app/src/main/java/com/nateshoffner/seachemdoser/ui/fragment/DefaultFragment.java
@@ -0,0 +1,35 @@
+package com.nateshoffner.seachemdoser.ui.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.nateshoffner.seachemdoser.R;
+
+public class DefaultFragment extends Fragment {
+
+    private View mRootView;
+
+    public DefaultFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        mRootView = inflater.inflate(R.layout.fragment_default, container, false);
+        return mRootView;
+    }
+
+}
