0aa67f02734a6d600b1951085dbc53eb0254871f
Nate Shoffner <nate.shoffner@gmail.com>
2016-03-30 13:39:12 -0400

    Missed in previous commit

--- app/src/main/java/com/nateshoffner/seachemdoser/core/model/SeachemProduct.java
+++ app/src/main/java/com/nateshoffner/seachemdoser/core/model/SeachemProduct.java
@@ -1,26 +1,40 @@
 package com.nateshoffner.seachemdoser.core.model;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Dictionary;
 import java.util.Hashtable;
+import java.util.List;
 
 public abstract class SeachemProduct implements Serializable {
 
     private String mName;
-    private String mComment;
+    private List<String> mComments = new ArrayList<>();
     private Dictionary<UnitMeasurement, SeachemParameter[]> mParameters = new Hashtable<>();
+    private List<String> mWarnings = new ArrayList<>();
 
-    public SeachemProduct(String name, String comment) {
+    public SeachemProduct(String name) {
         mName = name;
-        mComment = comment;
     }
 
     public String getName() {
         return mName;
     }
 
-    public String getComment() {
-        return mComment;
+    public void addComment(String comment) {
+        mComments.add(comment);
+    }
+
+    public String[] getComments() {
+        return mComments.toArray(new String[mComments.size()]);
+    }
+
+    public void addWarning(String warning) {
+        mWarnings.add(warning);
+    }
+
+    public String[] getWarnings() {
+        return mWarnings.toArray(new String[mWarnings.size()]);
     }
 
     public void setParameters(UnitMeasurement unitMeasurements, SeachemParameter[] params) {
