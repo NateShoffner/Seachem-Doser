e039797d9051fd0e4c47e4a3f4791a3a3fdd423f
Nate Shoffner <nate.shoffner@gmail.com>
2016-05-12 10:45:42 -0400

    Default fragment layout

--- app/src/main/java/com/nateshoffner/seachemdoser/ui/fragment/DefaultFragment.java
+++ app/src/main/java/com/nateshoffner/seachemdoser/ui/fragment/DefaultFragment.java
@@ -2,6 +2,7 @@ package com.nateshoffner.seachemdoser.ui.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -34,15 +35,22 @@ public class DefaultFragment extends Fragment {
         mRootView = inflater.inflate(R.layout.fragment_default, container, false);
 
         TextView tvVersion = (TextView) mRootView.findViewById(R.id.version);
-        tvVersion.append(getString(R.string.version_name));
+        tvVersion.setText(String.format("Version v%s",
+                getString(R.string.version_name)));
 
         Button btnStart = (Button)mRootView.findViewById(R.id.btnGetStarted);
         btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                ((DrawerActivity)getActivity()).openDrawer();
+                ((DrawerActivity) getActivity()).openDrawer();
             }
         });
+
+        TextView tvEula = (TextView)mRootView.findViewById(R.id.eula);
+        tvEula.setText(Html.fromHtml(getString(R.string.about_eula)));
+
+        TextView tvAuthor = (TextView)mRootView.findViewById(R.id.author);
+        tvAuthor.setText(Html.fromHtml(getString(R.string.about_author)));
 
         return mRootView;
     }
